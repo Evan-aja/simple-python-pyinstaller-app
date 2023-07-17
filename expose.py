1a5b85fd9671c35224da45492e0ffb53e62727df
Evan <gebel909@student.ub.ac.id>
2023-07-17 10:45:19 +0700

    jenkinsfile python test

---
+++ expose.py
@@ -0,0 +1,31 @@
+import subprocess
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def execute_binary(binary_path, args):
+    try:
+        cmd = [binary_path] + args
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        return stdout, stderr
+    except Exception as e:
+        return None, str(e)
+
+@app.route('/execute', methods=['POST'])
+def execute_endpoint():
+    data = request.get_json()
+    binary_path = data.get('binary_path', './add2vals')
+    args = data.get('args', [])
+
+    stdout, stderr = execute_binary(binary_path, args)
+
+    if stdout:
+        return jsonify({'stdout': stdout.decode('utf-8')})
+    elif stderr:
+        return jsonify({'stderr': stderr.decode('utf-8')})
+    else:
+        return jsonify({'error': 'Failed to execute the binary.'})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=12345)
